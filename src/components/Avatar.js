485b7b5cf26de9e20bb84f228aa149531a8e75df
Kate L <llewellyn.ksj@gmail.com>
2024-01-11 11:47:59 +0000

    Add avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,18 @@
+import styles from "../styles/Avatar.module.css";
+
+const Avatar = ({ src, height = 45, text }) => {
+  return (
+    <span>
+      <img
+        className={styles.Avatar}
+        src={src}
+        height={height}
+        width={height}
+        alt="profile image"
+      />
+      {text}
+    </span>
+  );
+};
+
+export default Avatar;
